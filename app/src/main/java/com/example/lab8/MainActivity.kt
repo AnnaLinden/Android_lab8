be0529f776917bb83c7677ac2db48f1708389605
AnnaLinden <linden.a.anna@gmail.com>
2024-09-18 08:32:16 +0300

    delete greeting function

--- app/src/main/java/com/example/lab8/MainActivity.kt
+++ app/src/main/java/com/example/lab8/MainActivity.kt
@@ -75,14 +75,6 @@ fun ImageFromUrl(url: URL, modifier: Modifier = Modifier) {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun ImagePreview() {
